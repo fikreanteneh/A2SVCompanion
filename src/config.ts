fd7885f946449266adca75c36a6cf6f9c6a58673
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2024-01-29 13:27:38 +0300

    Config Corected

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   api: {
-    url: 'https://a2-sv-companion-backend.vercel.app/api',
+    url: 'https://a2sv-remote.onrender.com/api',
     // url: 'http://localhost:5000/api',
   },
 };
